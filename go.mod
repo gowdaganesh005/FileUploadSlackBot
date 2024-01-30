590a96bbd25673687dd2c51a63d67f6af35b791f
gowdaganesh005 <gowdaganesh0511@gmail.com>
2024-01-31 00:01:59 +0530

    Basic upload file program

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/gowdaganesh005/FileUploadSlackBot
 
 go 1.21.5
 
-require (
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/slack-go/slack v0.12.3 // indirect
-)
+require github.com/slack-go/slack v0.12.3
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/slack-go/slack"
 )
 
 func main() {
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6555216986818-6548730551510-nHqT3m7OSqyWDYE2VlCMmgPP")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-6555216986818-6548730551510-p7xpV11mThmHD4jVIYsguRw5")
 	os.Setenv("CHANNEL_ID", "C06FWKUUP2B")
 
 	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
 	chnarr := []string{os.Getenv("CHANNEL_ID")}
-	filearr := []string{""}
+	filearr := []string{"README.md"}
+	for i := 0; i < len(filearr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: chnarr,
+			File:     filearr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("error:%v", err)
+		}
+		fmt.Printf("Name :%v URl: %v", file.Name, file.URL)
+	}
 
 }
